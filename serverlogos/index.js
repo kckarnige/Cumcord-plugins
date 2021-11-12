efc9031120d3a05bb90b50edd73439b317f0845e
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2021-11-11 19:04:14 -0500

    Update index.js

--- serverlogos/index.js
+++ serverlogos/index.js
@@ -1,24 +1,32 @@
-/// From StackOverflow: https://stackoverflow.com/questions/6390341/how-to-detect-if-url-has-changed-after-hash-in-javascript
-var oldURL = "";
-var currentURL = window.location.href;
+/// Courtesy of b1nzy :)
+import { injectCSS } from "@cumcord/patcher";
+
+let removeStyle;
+let clearURLChange = () => {};
+let oldURL = "";
+let currentURL = window.location.href;
+
 function checkURLchange(currentURL) {
     if (currentURL != oldURL) {
         oldURL = currentURL;
     }
 
     oldURL = window.location.href;
-    setTimeout(function () {
+    let timeoutID = setTimeout(() => {
         checkURLchange(window.location.href);
         document.getElementsByClassName("container-3w7J-x")[0].setAttribute('data-guild-id', window.location.pathname.split('/')[2]);
     }, 1);
+
+    clearURLChange = () => clearTimeout(timeoutID);
 }
 
-checkURLchange();
-
-var injectStyle = function () {
-    const style = document.createElement('style');
-    style.textContent = "@import url('https://kckarnige.is-a.dev/custom-server-logos/base.css');";
-    document.head.append(style);
-};
-
-injectStyle();
+export default {
+  onLoad() {
+    removeStyle = injectCSS(`@import url('https://kckarnige.is-a.dev/custom-server-logos/base.css');`);
+    checkURLChange();
+  },
+  onUnload() {
+    clearURLChange();
+    removeStyle();
+  }
+}

d45995e26e2b5febd95e66d0dbdbe6003154ee9d
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2022-06-03 12:48:29 -0400

    because why tf not lmao

---
+++ jquery-injector/index.js
@@ -0,0 +1,39 @@
+// "quick question: what the fuck why?" -Creatable 2022
+// Stole the toast code from Beefers, cope
+import { log } from '@cumcord/utils/logger';
+var pluginData = cumcord.pluginData.manifest;
+var showToast = cumcord.modules.webpack.findByProps("showToast").showToast;
+pluginData.id = "cumcord-jQuery";
+pluginData.enabled = function (boolean) {
+if (boolean === true) {
+  log(`${pluginData.name} | Injected!`)
+  showToast({message: `${pluginData.name} has been enabled! `, id: "plugin-toast", type: 1});
+} else if (boolean === false) {
+  log(`${pluginData.name} | Removed!`)
+  showToast({message: `${pluginData.name} has been disabled! `, id: "plugin-toast", type: 2});
+}
+}
+
+var injectScript = function () {
+    const script = document.createElement('script');
+    script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+    script.id = pluginData.id;
+    document.head.append(script);
+    pluginData.enabled(true)
+};
+
+
+var removeScript = function () {
+    var removeData = document.getElementById(pluginData.id);
+    removeData.parentNode.removeChild(removeData);
+    pluginData.enabled(false)
+};
+
+export default {
+  onLoad() {
+    injectScript();
+  },
+  onUnload() {
+    removeScript();
+  }
+}
--- serverlogos/index.js
+++ serverlogos/index.js
@@ -1,6 +1,7 @@
 // Thanks to creatable for the original URL check method :D
 
 import { log } from '@cumcord/utils/logger';
+var pluginData = cumcord.pluginData.manifest;
 const plugid = "serverlogos";
 const container_class = "container-1-ERn5";
 const headid = "serverlogos-logo";
@@ -8,23 +9,22 @@ const headid = "serverlogos-logo";
 var injectStyle = function () {
     const style = document.createElement('style');
     style.textContent = "@import url('https://kckarnige.is-a.dev/custom-server-logos/base.css');";
-    style.setAttribute('id', plugid);
+    style.id = plugid;
     document.head.append(style);
     document.getElementsByClassName(container_class)[0].setAttribute('id', headid);
-    log("[CSL] Injected CSS!")
+    log(`${pluginData.name} | Injected CSS!`)
 };
 
 
 var removeStyle = function () {
-    var ecsl = document. getElementById(plugid);
-    ecsl. parentNode. removeChild(ecsl);
-    
+    var removeData = document.getElementById(plugid);
+    removeData.parentNode.removeChild(removeData);    
 };
 
 var checkAttributeInjection = function () {
   if ((!window.location.pathname.split('/')[2] == "@me") && (!document.getElementsByClassName(container_class)[0].hasAttribute('data-guild-id'))) {
     document.getElementsByClassName(container_class)[0].setAttribute('data-guild-id', window.location.pathname.split('/')[2]);
-    log("[CSL] Injected attribute!");
+    log(`${pluginData.name} | Injected attribute!`);
     clearInterval();
   }
 };
@@ -39,6 +39,6 @@ export default {
     removeStyle();
     document.getElementById(headid).removeAttribute("data-guild-id");
     document.getElementById(headid).removeAttribute("id");
-    log("[CSL] Successfully disabled!")
+    log(`${pluginData.name} | Successfully disabled!`)
   }
 }

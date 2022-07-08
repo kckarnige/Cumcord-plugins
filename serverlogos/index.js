afc347f5ef3cc91eb984d6bfc8cdc39047051116
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2022-07-08 01:54:40 -0400

    Update index.js
    CSL overhaul

--- serverlogos/index.js
+++ serverlogos/index.js
@@ -1,44 +1,53 @@
-// Thanks to creatable for the original URL check method :D
-
 import { log } from '@cumcord/utils/logger';
 import { manifest as pluginData } from "@cumcord/pluginData";
-const plugid = "serverlogos";
+var showToast = cumcord.modules.webpack.findByProps("showToast").showToast;
+pluginData.id = pluginData.name.replace(/\s/g, '');
+pluginData.enabled = function (boolean) {
+if (boolean === true) {
+  log(`${pluginData.name} | Injected!`);
+  showToast({message: `${pluginData.name} has been enabled! `, id: "plugin-toast", type: 1});
+} else if (boolean === false) {
+  log(`${pluginData.name} | Removed!`);
+  showToast({message: `${pluginData.name} has been disabled! `, id: "plugin-toast", type: 2});
+}
+}
 const container_class = "container-1-ERn5";
-const headid = "serverlogos-logo";
+const logoid = pluginData.id + "-logo";
 
-var injectStyle = function () {
+var injectScript = function () {
+  pluginData.enabled(true)
     const style = document.createElement('style');
     style.textContent = "@import url('https://kckarnige.is-a.dev/custom-server-logos/base.css');";
-    style.id = plugid;
+    style.id = pluginData.id;
     document.head.append(style);
-    document.getElementsByClassName(container_class)[0].setAttribute('id', headid);
-    log(`${pluginData.name} | Injected CSS!`);
 };
 
 
-var removeStyle = function () {
-    var removeData = document.getElementById(plugid);
-    removeData.parentNode.removeChild(removeData);    
+var removeScript = function () {
+  pluginData.enabled(false)
+    var removeData = document.getElementById(pluginData.id);
+    removeData.parentNode.removeChild(removeData);
+    document.getElementById(logoid).removeAttribute("data-guild-id");
+    document.getElementById(logoid).removeAttribute("id", logoid);
 };
 
-var checkAttributeInjection = function () {
-  if ((!window.location.pathname.split('/')[2] == "@me") && (!document.getElementsByClassName(container_class)[0].hasAttribute('data-guild-id'))) {
+// Sacred saus
+var inrvul = setInterval(function () {
+  if ((!window.location.pathname.split('/')[2] == "@me") || (!document.getElementsByClassName(container_class)[0].hasAttribute('data-guild-id'))) {
     document.getElementsByClassName(container_class)[0].setAttribute('data-guild-id', window.location.pathname.split('/')[2]);
-    log(`${pluginData.name} | Injected attribute!`);
-    clearInterval();
+    document.getElementsByClassName(container_class)[0].id = logoid;
+    log(`${pluginData.name} | Injected guild ID attribute!`);
+    clearInterval(inrvul);
   }
-};
+}, 1);
 
-setInterval(checkAttributeInjection, 0);
+
 
 export default {
   onLoad() {
-    injectStyle();
+    injectScript();
   },
   onUnload() {
-    removeStyle();
-    document.getElementById(headid).removeAttribute("data-guild-id");
-    document.getElementById(headid).removeAttribute("id");
-    log(`${pluginData.name} | Successfully disabled!`);
+    removeScript();
   }
 }

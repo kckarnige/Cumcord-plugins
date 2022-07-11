79a8da0a9e0a1a18d5fcee3154e61a247ae0866b
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2022-07-10 23:36:46 -0400

    Update index.js

--- serverlogos/index.js
+++ serverlogos/index.js
@@ -3,44 +3,45 @@ import { manifest as pluginData } from "@cumcord/pluginData";
 var showToast = cumcord.modules.webpack.findByProps("showToast").showToast;
 pluginData.id = pluginData.name.replace(/\s/g, '');
 pluginData.enabled = function (boolean) {
-if (boolean === true) {
-  log(`${pluginData.name} | Injected!`);
-  showToast({message: `${pluginData.name} has been enabled! `, id: "plugin-toast", type: 1});
-} else if (boolean === false) {
-  log(`${pluginData.name} | Removed!`);
-  showToast({message: `${pluginData.name} has been disabled! `, id: "plugin-toast", type: 2});
-}
+  if (boolean === true) {
+    log(`${pluginData.name} | Injected!`);
+    showToast({ message: `${pluginData.name} has been enabled! `, id: "plugin-toast", type: 1 });
+  } else if (boolean === false) {
+    log(`${pluginData.name} | Removed!`);
+    showToast({ message: `${pluginData.name} has been disabled! `, id: "plugin-toast", type: 2 });
+  }
 }
 const container_class = "container-1-ERn5";
 const logoid = pluginData.id + "-logo";
 
 var injectScript = function () {
   pluginData.enabled(true)
-    const style = document.createElement('style');
-    style.textContent = "@import url('https://kckarnige.is-a.dev/custom-server-logos/base.css');";
-    style.id = pluginData.id;
-    document.head.append(style);
+  const style = document.createElement('style');
+  style.textContent = "@import url('https://kckarnige.is-a.dev/custom-server-logos/base.css');";
+  style.id = pluginData.id;
+  document.head.append(style);
 };
 
 
 var removeScript = function () {
   pluginData.enabled(false)
-    var removeData = document.getElementById(pluginData.id);
-    removeData.parentNode.removeChild(removeData);
-    document.getElementById(logoid).removeAttribute("data-guild-id");
-    document.getElementById(logoid).removeAttribute("id", logoid);
+  var removeData = document.getElementById(pluginData.id);
+  removeData.parentNode.removeChild(removeData);
+  document.getElementById(logoid).removeAttribute("data-guild-id");
+  document.getElementById(logoid).removeAttribute("id", logoid);
 };
 
-// Sacred saus
-var inrvul = setInterval(function () {
-  if ((!window.location.pathname.split('/')[2] == "@me") || (!document.getElementsByClassName(container_class)[0].hasAttribute('data-guild-id'))) {
-    document.getElementsByClassName(container_class)[0].setAttribute('data-guild-id', window.location.pathname.split('/')[2]);
-    document.getElementsByClassName(container_class)[0].id = logoid;
-    log(`${pluginData.name} | Injected guild ID attribute!`);
-    clearInterval(inrvul);
-  }
-}, 1);
 
+// Sacred saus
+setInterval(function () {
+  if (!(window.location.pathname.split('/')[2] == "@me")) {
+    if (!(document.getElementsByClassName(container_class)[0].hasAttribute('data-guild-id'))) {
+      document.getElementsByClassName(container_class)[0].setAttribute('data-guild-id', window.location.pathname.split('/')[2]);
+      document.getElementsByClassName(container_class)[0].id = logoid;
+      log(`${pluginData.name} | Injected guild ID attribute!`);
+    }
+  };
+}, 1);
 
 
 export default {
